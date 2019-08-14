123283968063607f58fc8b1c97ae077fdbad6db8
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-08-14 06:51:39 -0300

    Editando arquivos personalizados

--- wp-content/themes/themeGabriel/index.php
+++ wp-content/themes/themeGabriel/index.php
@@ -1,1 +1,21 @@
-Tema criado por Gabriel, primeira aula.
+<?php get_header(); ?>
+<?php get_header('personalizado'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <p>Tema criado por Gabriel</p>
+        </div>
+        <div class="col-md-3">
+            <?php get_sidebar(); ?>
+        </div>
+        <div class="col-md-3">
+            <?php get_sidebar('personalizado'); ?>
+        </div>
+    </div>
+
+</div>
+
+
+<?php get_footer('personalizado'); ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/themeGabriel/sidebar-personalizado.php
@@ -0,0 +1,1 @@
+<p>Outro Sidebar</p>
