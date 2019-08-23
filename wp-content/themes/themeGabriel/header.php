51b4d4c36f4905d85ebf710145319a347ad265ac
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-08-23 06:09:01 -0300

    Editando o campo busca

--- wp-content/themes/themeGabriel/header.php
+++ wp-content/themes/themeGabriel/header.php
@@ -49,8 +49,8 @@
    
     ?>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+    <form class="form-inline my-2 my-lg-0" action="<?php echo home_url('/'); ?>" method="get">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar" name="s" value="<?php echo get_search_query(); ?>">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
     </form>
   </div>
